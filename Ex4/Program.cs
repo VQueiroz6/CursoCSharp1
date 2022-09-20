e9020bdec8f6b03c95ed5f55367f6bf6e1883b37
VITOR QUEIROZ CARVALHO MENDONCA <vitor.qcmendonca@senacsp.edu.br>
2022-09-20 14:13:22 -0300

    Adicionar arquivos de projeto.

---
+++ Ex4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Fazer um programa para calcular o aumento de salário
+            de um funcionario x exibindo
+            - % de aumneto do salário
+            - Aumento em reais
+            - Salário Final
+            */
+
+            Console.WriteLine("======================================");
+            Console.WriteLine("=== Programa de aumento de salário ===");
+            Console.WriteLine("======================================");
+
+            Console.WriteLine("Insira o nome do funcionário: ");
+            string Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o salário do funcionário");
+            double SalarioInicial = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual % de aumento deseja para o funcionário " + Nome + "?");
+        }
+    }
+}
---
+++ Ex8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira sua idade");
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Maior de idade");
+
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+                Console.WriteLine("xxxxxxxxxxx Bloqueio xxxxxxxxxxxx");
+                Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+                Console.WriteLine("xxxxxxxx Menor de idade xxxxxxxxx");
+                Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+            }
+        }
+    }
+}
